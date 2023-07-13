929f48417456c2dded21796233b09c7a543b7d7b
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-07-13 17:27:50 +0200

    Add testing for getMobilesData function

---
+++ src/mocks/mocks.js
@@ -0,0 +1,19 @@
+export const mobileMockAcer = {
+  id: "ZmGrkLRPXOTpxsU4jjAcv",
+  brand: "Acer",
+  model: "Iconia Talk S",
+  price: "170",
+  imgUrl:
+    "https://itx-frontend-test.onrender.com/images/ZmGrkLRPXOTpxsU4jjAcv.jpg",
+};
+
+export const mobileMockAlcatel = {
+  id: "AasKFs5EGbyAEIKkcHQcF",
+  brand: "alcatel",
+  model: "Flash (2017)",
+  price: "100",
+  imgUrl:
+    "https://itx-frontend-test.onrender.com/images/AasKFs5EGbyAEIKkcHQcF.jpg",
+};
+
+export const mobileMocks = [mobileMockAcer, mobileMockAlcatel];
