84fb4fe83d40bbb933fb505f226200f94aecff03
victorandujar <victorandujarmurcia@gmail.com>
2023-07-16 11:04:38 +0200

    Add DetailPage route at router

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
 import HomePage from "../pages/HomePage/HomePage";
 import paths from "../utils/paths/paths";
+import DetailPage from "../pages/DetailPage/DetailPage";
 
 export const routes = [
   {
@@ -11,6 +12,10 @@ export const routes = [
       {
         path: paths.home,
         element: <HomePage />,
+      },
+      {
+        path: paths.detail,
+        element: <DetailPage />,
       },
     ],
   },
