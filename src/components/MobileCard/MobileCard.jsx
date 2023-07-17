6028cb7389048a46baf34eec4d99aba90b7cc9cf
victorandujar <victorandujarmurcia@gmail.com>
2023-07-17 16:28:35 +0200

    Add convert convertBrandFirstLetterIntoUpperCase function to MobileCard

--- src/components/MobileCard/MobileCard.jsx
+++ src/components/MobileCard/MobileCard.jsx
@@ -1,10 +1,21 @@
 import * as React from "react";
 import MobileCardStyled from "./MobileCardStyled";
 import { Link } from "react-router-dom";
+import endpoints from "../../api/endpoints";
 
 const MobileCard = ({ mobile }) => {
+  const convertBrandFirstLetterIntoUpperCase = (brand) => {
+    if (!brand) {
+      return "";
+    } else {
+      const firstLetterToUpperCase = brand.charAt(0).toUpperCase();
+      const restStringToLowerCase = brand.slice(1).toLowerCase();
+      return firstLetterToUpperCase + restStringToLowerCase;
+    }
+  };
+
   return (
-    <Link to={`/product/${mobile.id}`}>
+    <Link to={`${endpoints.products}/${mobile.id}`}>
       <MobileCardStyled className="mobile-card">
         <div className="mobile-card__image-container">
           <img
@@ -17,7 +28,7 @@ const MobileCard = ({ mobile }) => {
         </div>
         <div className="mobile-card__info info">
           <h2 className="info__mobile">
-            {mobile.brand} {mobile.model}
+            {convertBrandFirstLetterIntoUpperCase(mobile.brand)} {mobile.model}
           </h2>
           {!mobile.price ? (
             <span className="info__price">Not available</span>
