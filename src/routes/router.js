61373c19b6a33c21f0d1d6309c87a7954160c6f8
victorandujar <victorandujarmurcia@gmail.com>
2023-07-17 16:23:13 +0200

    Add ErrorPage to router to manage mistaken path pages

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,11 +3,13 @@ import App from "../components/App/App";
 import HomePage from "../pages/HomePage/HomePage";
 import paths from "../utils/paths/paths";
 import DetailPage from "../pages/DetailPage/DetailPage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 export const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: paths.home,
